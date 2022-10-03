7278d37af4aedb56621eac4394ce8cf9140ad3e3
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2022-10-03 16:27:55 -0700

    TEST File Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
+
+// The React Component is just a function which returns jsx
 function App() {
+  // Built-in State Concept - Re-evaluate the component
+  // That way to tell React to re-render 
+  
+  const [title, setTitle] = useState('Before Clicked.'); // returns an array
+
+  const clickHandler = () => {
+    console.log("Click for test..");
+    setTitle('After Clicked.')
+  };
+
   return (
     <div>
-      <div>Test</div>
+      <div>{title}</div>
+      {/* just point at the function, but execute, as adding event listeners in vanilla JS */}
+      <button onClick={clickHandler}>Test btn</button>
     </div>
   );
 }
 
-export default App;
+export default App; 
