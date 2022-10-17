8835f5cffe94cf3aae13978b82d8fbfd91b779ae
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2022-10-17 11:48:43 -0700

    useEffect, Login and Validation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  // Any change of the state will cause a re-render of the component
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Runs once if no change in dependencies
+  useEffect(() => {
+    // Check to stay user-loggin
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true); // Update the state - which triggers the conponent to render again
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // But it's just a dummy/demo anyways
+    // 1 - LoggedIn, 0 - Not LoggedIn
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
@@ -29,3 +42,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,3 +12,6 @@ const Home = (props) => {
 };
 
 export default Home;
+
+
+
