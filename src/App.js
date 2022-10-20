2b7098b8640f909a1cb2ddc64292b144a64c4260
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2022-10-19 17:09:15 -0700

    UserFinder.js added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
-import Users from './components/Users';
+import UserFinder from './components/UserFinder';
+// import Users from './components/Users';
 
 function App() {
-  return <Users></Users>
+
+  const date = new Date();
+  let weekString = '';
+
+  if (date.getDay() == 1) {
+    weekString = 'Mon';
+  } 
+  else if(date.getDay() == 2) {
+    weekString = 'Tue';
+  } 
+  else if (date.getDay() == 3) {
+    weekString = 'Wed';
+  }
+
+  console.log("Date: " + weekString);
+
+  return <UserFinder></UserFinder>
 }
 
 export default App;
