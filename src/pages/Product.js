0e245c5c8311319f308e381f236c6b56d3409dfe
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2022-11-14 15:47:31 -0800

    project init

---
+++ src/pages/Product.js
@@ -0,0 +1,14 @@
+const Products = () => {
+  return (
+    <section>
+      <h1>This is the Product Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+    return (
+      <h2>The Welcome Page</h2>
+    );
+}
+
+export default Welcome;
+
+
